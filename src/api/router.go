c874a527b13fcff42d293f300b15439a1f6865b9
saitooooooo <kitsit.39@gmail.com>
2022-12-11 14:46:16 +0900

    feat: sqlxでユーザー全取得

--- src/api/router.go
+++ src/api/router.go
@@ -1,17 +1,30 @@
 package api
 
 import (
-	"database/sql"
 	"net/http"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/saitooooooo/sqlx-xo-example/src/models"
 )
 
-func NewRouter(e *echo.Echo, db *sql.DB) {
+func NewRouter(e *echo.Echo, db *sqlx.DB) {
 	e.GET("/hello", hello)
 
+	e.GET("/user", getUserList(db))
 }
 
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World!!")
+}
+
+func getUserList(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var user []models.User
+		if err := db.Select(&user, "select * from users"); err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, user)
+	}
 }
